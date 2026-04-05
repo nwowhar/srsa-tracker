62b327e7833f49c886905ca4b7ac8c1f3949e3a2
nwowhar <nic.hardwick100@gmail.com>
2026-04-05 09:01:35 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+document.documentElement.style.background = '#0C0D10'
+document.body.style.background = '#0C0D10'
+document.body.style.margin = '0'
+document.body.style.padding = '0'
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
